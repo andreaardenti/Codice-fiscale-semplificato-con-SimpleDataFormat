4d6695c23bac31493701e36d716f4ed01940bf36
Andrea Ardenti <andreaardenti@users.noreply.github.com>
2018-05-25 15:30:59 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,10 @@
-//import java.text.SimpleDateFormat;
-
 public class Main {
 
     public static void main(String[] args) {
 
         User u = new User("andrea", "ardenti", "1972");
-
-        SimpleDateFormat a = new SimpleDateFormat("1992");
-
+        
         System.out.println(u);
-        System.out.println("Anno di nascita: " + a.getNascita());
-
-        System.out.println(u.getName() + u.getSurname() + u.getDateOfBirth());
-
+        System.out.println (u.getPrintCodFisc());
     }
 }
